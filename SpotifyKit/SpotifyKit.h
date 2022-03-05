093e7518e6e6be40b487cec9c10f892b0e234213
Afraz Siddiqui <afraz.siddiqui@microsoft.com>
2022-03-05 17:26:58 -0500

    Initial Commit

---
+++ SpotifyKit/SpotifyKit.h
@@ -0,0 +1,18 @@
+//
+//  SpotifyKit.h
+//  SpotifyKit
+//
+//  Created by Afraz Siddiqui on 3/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotifyKit.
+FOUNDATION_EXPORT double SpotifyKitVersionNumber;
+
+//! Project version string for SpotifyKit.
+FOUNDATION_EXPORT const unsigned char SpotifyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyKit/PublicHeader.h>
+
+
